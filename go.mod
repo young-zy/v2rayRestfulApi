99dc04aa7d8aa7eeda51688418b0f063b0927cd1
young-zy <youngzhaozhou@gmail.com>
2020-11-15 01:48:00 +0800

    Initial commit added stat api

---
+++ go.mod
@@ -0,0 +1,12 @@
+module v2rayRestfulApi
+
+go 1.15
+
+require (
+	github.com/golang/protobuf v1.4.3
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0
+	google.golang.org/genproto v0.0.0-20201113130914-ce600e9a6f9e // indirect
+	google.golang.org/grpc v1.33.2
+	google.golang.org/protobuf v1.25.0
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"net/http"
+	"v2rayRestfulApi/stats/command"
+)
+
+func runRestServer(endpoint string, listener net.Listener) error{
+	mux := runtime.NewServeMux()
+	dialOptions := []grpc.DialOption{grpc.WithInsecure()}
+	ctx := context.Background()
+	err := command.RegisterStatsServiceHandlerFromEndpoint(ctx, mux, endpoint, dialOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return http.Serve(listener, mux)
+}
+
+func main() {
+	port := flag.Int("port", 0, "restful server port")
+	endpoint := flag.String("endpoint", "", "endpoint of grpc server， example: 127.0.0.1:10086")
+	flag.Parse()
+	log.Printf("Listening on: %d",*port)
+	log.Printf("transforming data to: %s", *endpoint)
+	address := fmt.Sprintf("0.0.0.0:%d", *port)
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = runRestServer(*endpoint, listener)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
